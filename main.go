1b25f3ace5125179766fd584fb9493f9065e623f
andycheng <ccjeng@gmail.com>
2016-06-03 16:48:20 +0800

    init

---
+++ main.go
@@ -0,0 +1,62 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+var bot *linebot.Client
+
+func main() {
+	strID := os.Getenv("ChannelID")
+	numID, err := strconv.ParseInt(strID, 10, 64)
+	if err != nil {
+		log.Fatal("Wrong environment setting about ChannelID")
+	}
+
+	bot, err = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
+	log.Println("Bot:", bot, " err:", err)
+	http.HandleFunc("/callback", callbackHandler)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(addr, nil)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	received, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, result := range received.Results {
+		content := result.Content()
+		if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
+			text, err := content.TextContent()
+			_, err = bot.SendText([]string{content.From}, "OK "+text.Text)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
